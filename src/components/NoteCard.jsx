f2922512d515678ed6476c9a554beeaccb424c47
basith4me <abdulbasith174xyz@gmail.com>
2024-08-28 20:51:45 +0530

    initial commit

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,78 @@
+import React, { useRef, useEffect, useState } from "react";
+import Trash from "../icons/Trash";
+
+const NoteCard = ({ note }) => {
+  const body = JSON.parse(note.body);
+  const colors = JSON.parse(note.colors);
+  const [position, setPosition] = useState(JSON.parse(note.position));
+
+  let mouseStartPos = { x: 0, y: 0 };
+  const cardRef = useRef(null);
+
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  const autoGrow = (textarea) => {
+    const { current } = textAreaRef;
+    current.style.height = "auto"; // Reset the height
+    current.style.height = current.scrollHeight + "px"; // Set the new height
+  };
+
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+  };
+
+  const mouseMove = (e) => {
+    const mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    setPosition({
+      x: cardRef.current.offsetLeft - mouseMoveDir.x,
+      y: cardRef.current.offsetTop - mouseMoveDir.y,
+    });
+  };
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+      ref={cardRef}
+    >
+      <div
+        onMouseDown={mouseDown}
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          name=""
+          id=""
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
+
+export default NoteCard;
